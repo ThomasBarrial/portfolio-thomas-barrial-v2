384a35c3ea14b2e14f67555a22a7d31aac858e3a
Thomas Barrial <thomas.barrial@gmail.com>
2022-01-31 18:54:25 +0100

    test branch

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,19 +1,18 @@
-import React from "react";
+import React from 'react';
 
 function Loader({ loadAnim }: { loadAnim: string }): JSX.Element {
-    return (
-        <div
-            className={`h-screen w-screen bg-darkBlue z-50 fixed text-white ${loadAnim} flex flex-col items-center justify-center `}
-            style={{
-                backgroundImage: `url()`,
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-                backgroundRepeat: "no-repeat",
-            }}
-        >
-            loader
-        </div>
-    );
+  return (
+    <div
+      className={`h-screen w-screen bg-darkBlue z-50 fixed text-white ${loadAnim} flex flex-col items-center justify-center `}
+      style={{
+        backgroundImage: `url()`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+      }}>
+      qsd
+    </div>
+  );
 }
 
 export default Loader;
