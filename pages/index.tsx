9865f49c678e081ba3aacbf816e27f2db604c035
Thomas Barrial <thomas.barrial@gmail.com>
2022-01-31 19:03:18 +0100

    Landing integration ok / loader ok

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,26 @@
-// import { homeSections } from "../src/data/homepage/HomeSections";
-// import SectionContainer from "../src/components/SectionContainer";
-// import Layout from "../src/components/layout/Layout";
-// import Meta from "../src/components/SEO/Meta";
+import { homeSections } from '../src/data/homepage/HomeSections';
+import SectionContainer from '../src/components/SectionContainer';
+import Layout from '../src/components/layout/Layout';
+// import Meta from '../src/components/SEO/Meta';
 
 function Home(): JSX.Element {
-    return (
-        // <Layout data={homeSections}>
-        //     <Meta
-        //         pageTitle="Digital Copilote Website"
-        //         title={"Agence web & solutions d’innovations"}
-        //         description="Fort d’une équipe dynamique et engagée, notre agence développe des solutions adaptés et qui génèrent des résultats pour votre entreprise."
-        //         keywords="digitale, agence web, react, développement web, site vitrine, site sur mesure, blockchain, cryptomonnaie, react, typescript"
-        //     />
-        //     {homeSections.map((item) => {
-        //         return (
-        //             <SectionContainer key={item.id} id={item.id} BG={item.bg}>
-        //                 {item.Component()}
-        //             </SectionContainer>
-        //         );
-        //     })}
-        // </Layout>
-
-        <div className="h-screen w-screen flex items-center justify-center text-white">
-            HOMEPAGE
-        </div>
-    );
+  return (
+    <Layout data={homeSections}>
+      {/* <Meta
+        pageTitle="Digital Copilote Website"
+        title={'Agence web & solutions d’innovations'}
+        description="Fort d’une équipe dynamique et engagée, notre agence développe des solutions adaptés et qui génèrent des résultats pour votre entreprise."
+        keywords="digitale, agence web, react, développement web, site vitrine, site sur mesure, blockchain, cryptomonnaie, react, typescript"
+      /> */}
+      {homeSections.map((item) => {
+        return (
+          <SectionContainer key={item.id} id={item.id} BG={item.bg}>
+            {item.Component()}
+          </SectionContainer>
+        );
+      })}
+    </Layout>
+  );
 }
 
 export default Home;
