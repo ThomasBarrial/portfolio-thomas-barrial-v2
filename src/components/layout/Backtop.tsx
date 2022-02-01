446631a4faac70ed892f0076697a65a2a57a6e84
Thomas Barrial <thomas.barrial@gmail.com>
2022-02-01 16:40:17 +0100

    roadmap ok

--- src/components/layout/Backtop.tsx
+++ src/components/layout/Backtop.tsx
@@ -1,47 +1,37 @@
-import React, { useState } from "react";
-import Image from "next/dist/client/image";
+import React, { useState } from 'react';
+import Image from 'next/dist/client/image';
 
 function Backtop(): JSX.Element {
-    const [visible, setVisible] = useState(false);
+  const [visible, setVisible] = useState(false);
 
-    const toggleVisible = () => {
-        const scrolled = document.documentElement.scrollTop;
-        if (scrolled > 300) {
-            setVisible(true);
-        } else if (scrolled <= 300) {
-            setVisible(false);
-        }
-    };
-
-    const scrollToTop = () => {
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth",
-        });
-    };
-    if (typeof window !== "undefined") {
-        // browser code
-        window.addEventListener("scroll", toggleVisible);
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop;
+    if (scrolled > 300) {
+      setVisible(true);
+    } else if (scrolled <= 300) {
+      setVisible(false);
     }
+  };
 
-    return (
-        <button
-            type="button"
-            onClick={() => scrollToTop()}
-            className="flex items-center fixed bottom-8 text-pink right-44 lg:right-56"
-        >
-            {visible && (
-                <p className="hidden animate-fadeIn lg:flex">Haut de page</p>
-            )}
-            <div
-                className={`ml-2  ${
-                    visible ? `transform duration-700 rotate-0 mt-2` : `hidden`
-                }`}
-            >
-                <Image src={"/icons/arrow.svg"} height={20} width={20} alt="" />
-            </div>
-        </button>
-    );
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+  if (typeof window !== 'undefined') {
+    // browser code
+    window.addEventListener('scroll', toggleVisible);
+  }
+
+  return (
+    <button type="button" onClick={() => scrollToTop()} className="flex items-center fixed bottom-8 text-pink right-44 lg:right-56">
+      {visible && <p className="hidden animate-fadeIn lg:flex">Haut de page</p>}
+      <div className={`ml-2  ${visible ? `transform duration-700 rotate-0 mt-2` : `hidden`}`}>
+        <Image src={'/icons/arrow.svg'} height={20} width={20} alt="" />
+      </div>
+    </button>
+  );
 }
 
 export default Backtop;
--- src/components/layout/Indicator.tsx
+++ src/components/layout/Indicator.tsx
@@ -1,55 +1,44 @@
-import React, { ReactElement, useContext } from "react";
-import { motion } from "framer-motion";
-import { AppContext } from "../../context/AppContext";
+import React, { ReactElement, useContext } from 'react';
+import { motion } from 'framer-motion';
+import { AppContext } from '../../context/AppContext';
 
 interface IProps {
-    data: ISection[];
+  data: ISection[];
 }
 export default function Indicator({ data }: IProps): ReactElement {
-    const { state } = useContext(AppContext);
-    const variants = {
-        open: { scaleX: 1.3, height: "1px", opacity: 1 },
-        close: { scaleX: 1, height: "0px", opacity: 0.3 },
-    };
+  const { state } = useContext(AppContext);
+  const variants = {
+    open: { scaleX: 1.3, height: '1px', opacity: 1 },
+    close: { scaleX: 1, height: '0px', opacity: 0.3 },
+  };
 
-    const handleClick = (id: string) => {
-        const element = document.querySelector(`#${id}`);
-        if (element) {
-            element.scrollIntoView({ behavior: "smooth" });
-        }
-    };
+  const handleClick = (id: string) => {
+    const element = document.querySelector(`#${id}`);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
 
-    return (
-        <div
-            className={`hidden w-32 right-5 top-5 lg:left-10 lg:top-44 fixed lg:flex flex-col items-center align-middle text-black justify-around `}
-        >
-            {data.map((section, index) => {
-                return (
-                    <button
-                        type="button"
-                        onClick={() => handleClick(section.id)}
-                        className="w-full flex justify-end"
-                        key={index}
-                    >
-                        {state.index === index + 1 ? (
-                            <div className="flex items-center w-5/12 text-xs opacity-50 font-syncopate font-bold text-pink">
-                                <p className="text-sm">{state.index}</p>/
-                                {data.length}
-                            </div>
-                        ) : (
-                            ""
-                        )}
-                        <motion.span
-                            initial="close"
-                            animate={
-                                state.index === index + 1 ? "open" : "close"
-                            }
-                            variants={variants}
-                            className={`border border-pink w-5/12 relative   my-4`}
-                        ></motion.span>
-                    </button>
-                );
-            })}
-        </div>
-    );
+  return (
+    <div className={`hidden w-32 right-5 top-5 lg:left-10 lg:top-44 fixed lg:flex flex-col items-center align-middle text-white justify-around `}>
+      {data.map((section, index) => {
+        return (
+          <button type="button" onClick={() => handleClick(section.id)} className="w-full flex justify-end" key={index}>
+            {state.index === index + 1 ? (
+              <div className="flex items-center w-5/12 text-xs opacity-50 font-syncopate font-bold text-pink">
+                <p className="text-sm">{state.index}</p>/{data.length}
+              </div>
+            ) : (
+              ''
+            )}
+            <motion.span
+              initial="close"
+              animate={state.index === index + 1 ? 'open' : 'close'}
+              variants={variants}
+              className={`border border-pink w-5/12 relative   my-4`}></motion.span>
+          </button>
+        );
+      })}
+    </div>
+  );
 }
