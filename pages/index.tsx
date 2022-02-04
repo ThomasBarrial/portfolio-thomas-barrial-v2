e2b606a518466a0572d223aa29732a1bb0b142f7
Thomas Barrial <thomas.barrial@gmail.com>
2022-02-04 11:14:10 +0100

    add meta and header ok

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,18 @@
 import { homeSections } from '../src/data/homepage/HomeSections';
 import SectionContainer from '../src/components/SectionContainer';
 import Layout from '../src/components/layout/Layout';
+import Meta from '../src/components/SEO/Meta';
 // import Meta from '../src/components/SEO/Meta';
 
 function Home(): JSX.Element {
   return (
     <Layout data={homeSections}>
-      {/* <Meta
-        pageTitle="Digital Copilote Website"
-        title={'Agence web & solutions d’innovations'}
-        description="Fort d’une équipe dynamique et engagée, notre agence développe des solutions adaptés et qui génèrent des résultats pour votre entreprise."
-        keywords="digitale, agence web, react, développement web, site vitrine, site sur mesure, blockchain, cryptomonnaie, react, typescript"
-      /> */}
+      <Meta
+        pageTitle="SpendCoin"
+        title={'Buy Everything with crypto'}
+        description="Spend coin team is focus on building customised e-shops called meta-shops. As a seller, you’ll be able to offer decentralized crypto-paiement to your customers and receive euros or crypto-currencies at your will."
+        keywords=""
+      />
       {homeSections.map((item) => {
         return (
           <SectionContainer key={item.id} id={item.id} BG={item.bg}>
