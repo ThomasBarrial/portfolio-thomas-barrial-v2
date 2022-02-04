393334482f03f6336d6e566222996617198bfff0
Thomas Barrial <thomas.barrial@gmail.com>
2022-02-04 11:49:54 +0100

    add favicon in links

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,6 +14,7 @@ class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link rel="icon" href="/favicon.ico" />
           <link href="https://fonts.googleapis.com/css2?family=Syncopate:wght@400;700&display=swap" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
         </Head>
